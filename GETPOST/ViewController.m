136dbe28fe42398bebaa3cd119e91776a66e2912
林柏参 <sshare@qq.com>
2014-10-21 14:53:01 +0800

    Initial Commit

---
+++ GETPOST/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GETPOST
+//
+//  Created by 林柏参 on 14/10/21.
+//  Copyright (c) 2014年 林柏参. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
