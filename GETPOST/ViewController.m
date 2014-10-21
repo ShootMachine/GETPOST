00fee199fb2a0f8451c23ce34022542a5bc60770
林柏参 <sshare@qq.com>
2014-10-21 14:55:19 +0800

    带缓存的 get 请求
    写了 Demo 写了注视

--- GETPOST/ViewController.m
+++ GETPOST/ViewController.m
@@ -1,12 +1,13 @@
 //
 //  ViewController.m
-//  GETPOST
+//  Demo
 //
 //  Created by 林柏参 on 14/10/21.
 //  Copyright (c) 2014年 林柏参. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "BaseHttpTool.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  带缓存的get请求   需要导入 sql 框架
+     */
+    NSString *url = @"www.xxx.com";
+    
+    [BaseHttpTool getCacheWithUrl:url parameters:nil sucess:^(NSDictionary *json) {
+        
+        NSLog(@"json - %@",json);
+        
+    } failur:^(NSError *error) {
+        
+        NSLog(@"error - %@",[error description]);
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
